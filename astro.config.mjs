0aca7d7fed33d021e2aa2613ee63f1166343fb5b
GonzDv <xliqn0@gmail.com>
2026-07-22 12:48:54 -0600

    fix: GitHub has not accepted the images or styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Tu dominio principal de GitHub Pages
+  site: 'https://TU-gonzdv.github.io', 
+  
+  // 2. El nombre EXACTO de tu repositorio (con la barra al inicio)
+  base: '/GSAP--ScrollTrigger-',
   vite: {
     plugins: [tailwindcss()],
   },
